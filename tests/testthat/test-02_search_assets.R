6938da08766555a5d042652c177edb393610aeae
alex23lemm <alex23lemm@gmail.com>
2019-12-18 16:27:38 +0100

    Rename test files

--- tests/testthat/test-02_search_assets.R
+++ tests/testthat/test-02_search_assets.R
@@ -1,8 +1,10 @@
 context("asset search")
 
+token <- tm_get_token()
+
 test_that("tm_get_tags() returns a data frame with all tags", {
   skip_on_cran()
-  token <- tm_get_token()
+
   tags <- tm_get_tags(token)
   expect_identical(dim(tags), c(59L, 17L))
 })

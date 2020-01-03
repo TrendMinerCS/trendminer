535ddc13188870d6a3f57225df2fc8213fd5098a
alex lemm <alex23lemm@gmail.com>
2020-01-03 14:10:40 +0100

    Add pagination and unit tests to tm_child_structures()

--- R/browse_structures.R
+++ R/browse_structures.R
@@ -74,6 +74,7 @@ tm_root_structures <- function(token, ...) {
     select_structure_result_columns()
 }
 
+
 select_structure_result_columns <- function(df) {
   df %>%
     dplyr::select(-.data$links) %>%
@@ -82,24 +83,48 @@ select_structure_result_columns <- function(df) {
 }
 
 
-#' Get child structures by parent structure Id
+#' Get child structures by parent structure ID
 #'
-#' Gets the structures that have a parent defined by parentId and returns them
+#' Gets the structures that have a parent defined by `parent_id` and returns them
 #' in a data frame.
 #'
-#' @param parentId Parent structure Id
+#' @details
+#' Depending on the number of child structures, the TrendMiner response might be paginated.
+#' `tm_child_structures()` manages pagination completely on its own by combining all
+#' paginated child structure results in a data frame before returning them.
+#'
+#'
+#' @param parent_id Parent structure ID
 #' @inheritParams tm_root_structures
-#' @return A data frame
+#' @return A data frame with child structures of `parent_id`.
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' token <- tm_token()
 #'
-#' tm_child_structures(token)
+#' # Retrieve child structures of the first root structure
+#' roots <- tm_root_structures(token)
+#' tm_child_structures(token, roots[1, "structureId"])
+#'
+#' # Get child structures by specific parent structure ID
+#' tm_child_structures(token, "e5225244-c6de-48c2-87da-5b51b65062e8")
 #' }
-tm_child_structures <- function(token, parentId, ...) {
-  url <- paste(token$base_url, "/af/assets/browse?parentId=", parentId, sep = "")
+tm_child_structures <- function(token, parent_id, ...) {
+
+  if (class(token) != "tm_token") {
+    stop("'token' must be a TrendMiner access token.")
+  }
+
+  if (class(token) == "tm_token" && !tm_is_valid_token(token)) {
+    stop("Token expired. Please provide a valid access token.")
+  }
+
+  if (length(parent_id) != 1L || typeof(parent_id) != "character") {
+    stop("'parent_id' must be a length-one character vector.")
+  }
+
+  url <- paste(token$base_url, "/af/assets/browse?parentId=", parent_id, sep = "")
 
   response <- httr::GET(url,
                         httr::add_headers(Authorization = paste("Bearer", token$access_token, sep = "")),
@@ -110,17 +135,59 @@ tm_child_structures <- function(token, parentId, ...) {
   parsed <- httr::content(response, as =  "text", encoding = "UTF-8") %>%
     jsonlite::fromJSON()
 
-  content <- parsed$content
   # check if structure represents leaf structure. If yes, return empty list
-  if (is.list(content) & length(content) == 0)
-      return(content)
+  if (is.list(parsed$content) & length(parsed$content) == 0)
+    return(parsed$content)
 
-  content %>%
+  total_pages <- parsed$page$totalPages
+
+  content <- vector("list", total_pages)
+  content[[1]] <- parsed$content
+
+  if (total_pages == 1) {
+    content <- content[[1]] %>%
+      select_structure_result_columns()
+    return(content)
+  }
+
+  # deal with pagination
+  for(i in 2:length(content)) {
+
+    next_link <- parsed$links %>%
+      dplyr::filter(.data$rel == "next") %>%
+      dplyr::select(.data$href) %>%
+      unlist(.[1,]) %>%
+      unname()
+
+    response <- httr::GET(next_link,
+                          httr::add_headers(Authorization = paste("Bearer", token$access_token, sep = "")),
+                          httr::user_agent(tm_get_useragent()),
+                          httr::accept_json(),
+                          ...)
+
+    if (httr::http_error(response)) {
+      stop(
+        sprintf(
+          "TrendMiner API request failed [%s]\n%s\n%s\n%s",
+          httr::status_code(response),
+          httr::http_status(response)$category,
+          httr::http_status(response)$reason,
+          httr::http_status(response)$message
+        ),
+        call. = FALSE
+      )
+    }
+    parsed <- httr::content(response, as =  "text", encoding = "UTF-8") %>%
+      jsonlite::fromJSON()
+    content[[i]] <- parsed$content
+  }
+  dplyr::bind_rows(content) %>%
     select_structure_result_columns()
 }
 
+#'
+#'
 tm_descendant_structures <- function(token, parentId) {
-
   content <- tm_child_structures(token, parentId)
 
   if (is.list(content) & length(content) == 0)
@@ -130,6 +197,5 @@ tm_descendant_structures <- function(token, parentId) {
     content <- dplyr::bind_rows(content,
                                 tm_descendant_structures(token, content$structureId[i]))
   }
-
  content
 }
--- tests/testthat/test-03_browse_structures.R
+++ tests/testthat/test-03_browse_structures.R
@@ -2,7 +2,7 @@ context("browse structures")
 
 token <- tm_token()
 
-test_that("tm_root_structures returns expected results", {
+test_that("tm_root_structures() returns the expected results", {
   skip_on_cran()
 
   response <- tm_root_structures(token)
@@ -17,4 +17,40 @@ test_that("tm_root_structures returns expected results", {
                                          "15529c52-ae81-4a23-a988-91e6551cb1a7",
                                          "876381a3-3dac-42f8-b72b-eac9d7be42b8"),
                               stringsAsFactors = FALSE))
+})
+
+test_that("tm_child_structures() returns the expected results", {
+  skip_on_cran()
+
+  response <- tm_child_structures(token, "2b66622f-83c7-48e9-9ccc-6dd9214e70c6")
+  expect_identical(dim(response), c(2L, 14L))
+  expect_identical(response[["structureId"]], c("1aef0aa1-f942-441e-82d8-0c4bfe7208b3",
+                                              "4e58e3ca-e57d-47b5-8619-20d39626116e"))
+  response <- tm_child_structures(token, "f8ae4783-c265-4d88-8e4c-59d7942a9385")
+  expect_identical(dim(response), c(9L, 16L))
+})
+
+
+test_that("tm_child_structures() returns error if token is not of class 'tm_token'", {
+  skip_on_cran()
+
+  expect_error(tm_child_structures("not_a_token", "4e58e3ca-e57d-47b5-8619-20d39626116e"),
+               "'token' must be a TrendMiner access token.")
+})
+
+test_that("tm_child_structures() returns error if an invalid token is used", {
+  skip_on_cran()
+
+  deprecated_token <- token
+  deprecated_token$expiration_date <- deprecated_token$expiration_date - 43201
+  expect_error(tm_child_structures(deprecated_token, "4e58e3ca-e57d-47b5-8619-20d39626116e"),
+               "Token expired. Please provide a valid access token.")
+})
+
+test_that("tm_child_structures() returns error if 'parent_id' is not length one character vector", {
+  skip_on_cran()
+
+  expect_error(tm_child_structures(token, 1), "'parent_id' must be a length-one character vector.")
+  expect_error(tm_child_structures(token, c("not", "a", "single", "parent_id")),
+               "'parent_id' must be a length-one character vector")
 })
